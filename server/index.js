a0fe5592b8790ed2d87543ed3f205bd319a9d4cb
Jesse Etherington <jesse.etherington@gmail.com>
2021-05-04 10:05:28 -0600

    added server files

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const {SERVER_PORT} = process.env
+const path = require('path')
+const app = express()
+
+app.use(express.json())
+
+
+app.listen(SERVER_PORT, () => console.log(`server listening on port ${SERVER_PORT}`))
+
+app.use(express.static(`${__dirname}/../build`))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+})
